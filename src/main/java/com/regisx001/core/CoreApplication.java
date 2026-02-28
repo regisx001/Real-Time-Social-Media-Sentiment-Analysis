b648bd625061e365a5a98986f8e9b5bb68f112b4
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2026-02-28 20:07:36 +0000

    initial

---
+++ src/main/java/com/regisx001/core/CoreApplication.java
@@ -0,0 +1,16 @@
+package com.regisx001.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
