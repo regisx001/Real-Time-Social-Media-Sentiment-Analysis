7b957bea1e7747d16dd7538dbbf487a09c71dc6a
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2026-03-03 21:37:18 +0000

    feat: Improve formatting of theme toggle button in sentiment dashboard header

--- web/src/lib/components/sentiment-site-header.svelte
+++ web/src/lib/components/sentiment-site-header.svelte
@@ -51,8 +51,12 @@
         {/if}
       </div>
       <Button onclick={toggleMode} variant="outline" size="icon">
-        <Sun class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-        <Moon class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <Sun
+          class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+        />
+        <Moon
+          class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+        />
         <span class="sr-only">Toggle theme</span>
       </Button>
     </div>
